404c76b6e58f3870b1b5ebca40052f4ede4493c8
NayeliGon <125329142+NayeliGon@users.noreply.github.com>
2024-11-05 18:44:55 -0600

    Update credenciales_validas.py

--- tests/credenciales_validas.py
+++ tests/credenciales_validas.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 
-service = Service(ChromeDriverManager().install())
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(service=service)
 
-driver = webdriver.Chrome(service=service)
 
 try:
 
